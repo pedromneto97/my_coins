1233c518f7f261989cccf52c753afa250359564c
Pedro Neto <pedromneto97@gmail.com>
2023-08-01 23:49:09 -0300

    feat: add login page

---
+++ lib/src/presentation/auth/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LoginPage'),
+      ),
+    );
+  }
+}
