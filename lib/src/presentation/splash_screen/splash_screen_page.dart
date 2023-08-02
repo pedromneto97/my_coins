90de8941594af7b49f376dbff6cec0c85fdbeeca
Pedro Neto <pedromneto97@gmail.com>
2023-08-02 20:17:24 -0300

    feat: add redirect logic at splash screen

--- lib/src/presentation/splash_screen/splash_screen_page.dart
+++ lib/src/presentation/splash_screen/splash_screen_page.dart
@@ -1,9 +1,30 @@
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../../routes/app_router.dart';
+
 @RoutePage()
-class SplashScreenPage extends StatelessWidget {
+class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({super.key});
+
+  @override
+  State<SplashScreenPage> createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => _checkAuthAndRedirect());
+  }
+
+  void _checkAuthAndRedirect() {
+    final route = FirebaseAuth.instance.currentUser == null ? const LoginRoute() : const HomeRoute();
+
+    context.replaceRoute(route);
+  }
 
   @override
   Widget build(BuildContext context) {
