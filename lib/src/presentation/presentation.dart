f784412fd9417d93f6cb29290ee7f50af4dcbcdc
Pedro Neto <pedromneto97@gmail.com>
2023-08-01 23:49:23 -0300

    feat: add routes

---
+++ lib/src/presentation/presentation.dart
@@ -0,0 +1,2 @@
+export 'auth/login_page.dart';
+export 'splash_screen/splash_screen_page.dart';
---
+++ lib/src/routes/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:auto_route/auto_route.dart';
+
+import '../presentation/presentation.dart';
+
+part 'app_router.gr.dart';
+
+@AutoRouterConfig(
+  replaceInRouteName: 'Page,Route',
+)
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(
+          page: SplashScreenRoute.page,
+          initial: true,
+        ),
+        AutoRoute(
+          page: LoginRoute.page,
+        ),
+      ];
+}
