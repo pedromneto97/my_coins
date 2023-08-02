602c090841c2d30bd46c9aa5537847f7df5fc5de
Pedro Neto <pedromneto97@gmail.com>
2023-08-02 20:17:53 -0300

    feat: setup firebase ui auth logic

--- lib/src/setup.dart
+++ lib/src/setup.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/widgets.dart';
 
 import '../firebase_options.dart';
@@ -13,4 +15,9 @@ Future<void> _setupFirebase() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseUIAuth.configureProviders([
+    EmailAuthProvider(),
+    GoogleProvider(clientId: '38384210019-1ggtoqnlbt7pjrpq9ed368mn5jt0bd3t.apps.googleusercontent.com'),
+  ]);
 }
