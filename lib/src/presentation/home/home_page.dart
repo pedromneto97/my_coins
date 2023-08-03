797da63a1ee0a1cbbf9d76a8383387104b46d82d
Pedro Neto <pedromneto97@gmail.com>
2023-08-02 21:39:02 -0300

    feat: add home entries

--- lib/src/presentation/home/home_page.dart
+++ lib/src/presentation/home/home_page.dart
@@ -1,15 +1,51 @@
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+
+import '../../routes/app_router.dart';
+import '../../utils/localizations.dart';
 
 @RoutePage()
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void _onTapProfile(BuildContext context) => context.router.push(const ProfileRoute());
+
+  void _onTapMyCollection(BuildContext context) {}
+
+  void _onTapCollections(BuildContext context) {}
+
   @override
   Widget build(BuildContext context) {
+    final strings = context.strings;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bem vindo!'),
+        title: Text(strings.welcome),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            title: Text(strings.myCollection),
+            subtitle: Text(strings.myCollectionDescription),
+            leading: const Icon(Icons.collections_bookmark_rounded),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
+          ),
+          const Divider(),
+          ListTile(
+            title: Text(strings.collections),
+            subtitle: Text(strings.collectionsDescription),
+            leading: const Icon(Icons.list),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
+          ),
+          const Divider(),
+          ListTile(
+            title: Text(strings.profile),
+            leading: const Icon(Icons.person),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
+            onTap: () => _onTapProfile(context),
+          ),
+          const Divider(),
+        ],
       ),
     );
   }
