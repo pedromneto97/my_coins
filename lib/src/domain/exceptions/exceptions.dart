6bb17d506946ecc7a62ff4d71b6233444ba30f91
Pedro Neto <pedromneto97@gmail.com>
2023-08-03 22:08:19 -0300

    feat: add exceptions

--- lib/src/domain/exceptions/exceptions.dart
+++ lib/src/domain/exceptions/exceptions.dart
@@ -9,3 +9,43 @@ class FailedToRemoveCoinException implements MyCoinsException {
 class FailedToAddCoinException implements MyCoinsException {
   const FailedToAddCoinException();
 }
+
+class TemplateNotFoundException implements MyCoinsException {
+  const TemplateNotFoundException();
+}
+
+class FailedToGetTemplatesException implements MyCoinsException {
+  const FailedToGetTemplatesException();
+}
+
+class FailedToCreateCollectionException implements MyCoinsException {
+  const FailedToCreateCollectionException();
+}
+
+class FailedToDeleteCollectionException implements MyCoinsException {
+  const FailedToDeleteCollectionException();
+}
+
+class CollectionNotFoundException implements MyCoinsException {
+  const CollectionNotFoundException();
+}
+
+class FailedToGetPublicCollectionsException implements MyCoinsException {
+  const FailedToGetPublicCollectionsException();
+}
+
+class FailedToGetUserCollectionsException implements MyCoinsException {
+  const FailedToGetUserCollectionsException();
+}
+
+class FailedToUpdateCollectionException implements MyCoinsException {
+  const FailedToUpdateCollectionException();
+}
+
+class FailedToAddCoinToCollectionException implements MyCoinsException {
+  const FailedToAddCoinToCollectionException();
+}
+
+class FailedToRemoveCoinFromCollectionException implements MyCoinsException {
+  const FailedToRemoveCoinFromCollectionException();
+}
