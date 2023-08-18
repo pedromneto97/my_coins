f65b9e31b66b261c601e6a7a42deb46fc68fe027
Pedro Neto <pedromneto97@gmail.com>
2023-08-17 21:53:09 -0300

    feat: update collection when event is emitted

--- lib/src/presentation/user_collections/cubit/get_user_collections/get_user_collections_cubit.dart
+++ lib/src/presentation/user_collections/cubit/get_user_collections/get_user_collections_cubit.dart
@@ -1,17 +1,33 @@
+import 'dart:async';
+
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../domain/domain.dart';
+import '../../../../shared/event_bus/event_bus.dart';
+import '../../../../shared/event_bus/events/collection_updated_event.dart';
 
 part 'get_user_collections_state.dart';
 
 class GetUserCollectionsCubit extends Cubit<GetUserCollectionsState> {
   final GetUserCollectionsUseCase _useCase;
+  late final StreamSubscription _subscription;
 
   GetUserCollectionsCubit({
     required GetUserCollectionsUseCase useCase,
   })  : _useCase = useCase,
-        super(const GetUserCollectionsInitial());
+        super(const GetUserCollectionsInitial()) {
+    _subscription = EventBus.I.listen<CollectionUpdatedEvent>(
+      (event) => _updateCollection(event.collection),
+    );
+  }
+
+  @override
+  Future<void> close() {
+    _subscription.cancel();
+
+    return super.close();
+  }
 
   Future<void> getUserCollections() async {
     emit(const GetUserCollectionsLoading());
@@ -23,6 +39,18 @@ class GetUserCollectionsCubit extends Cubit<GetUserCollectionsState> {
       );
     } catch (_) {
       emit(const GetUserCollectionsError());
+    }
+  }
+
+  void _updateCollection(Collection collection) {
+    final state = this.state;
+    if (state is GetUserCollectionsLoaded) {
+      final collections = [...state.collections];
+      final index = collections.indexWhere((element) => element.id == collection.id);
+      if (index != -1) {
+        collections[index] = collection;
+        emit(GetUserCollectionsLoaded(collections: collections));
+      }
     }
   }
 }
